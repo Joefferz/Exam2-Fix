6ec66152a1718e94e933c4a25575cd73a8c2e1aa
Joeffrey - 2513172 <brionesjoeffrey@gmail.com>
2025-04-25 09:05:51 -0400

    Initial sharing of project

---
+++ Fix.java
@@ -0,0 +1,56 @@
+
+/**
+ * Exam2-Fix.
+ *
+ * @author Joeffrey
+ */
+public class Fix
+{
+    public Fix()
+    {
+        
+    }
+    
+    public void sampleMethod(int y)
+    {
+        //Question 24
+        /*
+            String a = "Hello"; //a)
+            String b = "Hello";
+            if (a.equals(b))    //b)
+            {
+                return "Correct";   //c)
+            }
+            else
+            {
+                return "Incorrect";
+            }
+           */
+    }
+    
+    //question 25
+    /*
+    public double get maxTemperature(ArrayList<Temperature> temps)
+    {
+        for (double temp: temps)
+        {
+            if (temp > 37,5)
+            {
+                double max = temp;
+            }
+        }
+        if (max < temp)
+        {
+            double newMax = temp;
+            return newMax;
+        }
+    }
+       
+       */
+      
+    //question 26
+    //temps.max();
+    
+    //question 27
+    //maxTemp.sort(temps.max());
+}
