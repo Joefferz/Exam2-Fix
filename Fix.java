a1ad603391000220d30f6570bc42c3d8cdc9a02a
Joeffrey - 2513172 <brionesjoeffrey@gmail.com>
2025-04-25 09:45:03 -0400

    Question 25 - Incomplete

--- Fix.java
+++ Fix.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Exam2-Fix.
  *
@@ -8,45 +8,51 @@ public class Fix
 {
     public Fix()
     {
+        ArrayList<Double> temperatures = new ArrayList<Double>();
         
+        temperatures.add(36.5);
+        temperatures.add(38.5);
+        temperatures.add(45.5);
+        temperatures.add(30.5);
+        temperatures.add(50.5);
+        temperatures.add(57.5);
+        temperatures.add(25.5);
+        
+        getMaxTemperature(temperatures);
     }
     
-    public void sampleMethod(int y)
+    public String sampleMethod()
     {
         //Question 24
-        /*
-            String a = "Hello"; //a)
-            String b = "Hello";
-            if (a.equals(b))    //b)
-            {
-                return "Correct";   //c)
-            }
-            else
-            {
-                return "Incorrect";
-            }
-           */
+        
+        String a = "Hello"; //a)
+        String b = "Hello";
+        if (a.equals(b))    //b)
+        {
+            return "Correct";   //c)
+        }
+        else
+        {
+            return "Incorrect";
+        }
     }
     
     //question 25
-    /*
-    public double get maxTemperature(ArrayList<Temperature> temps)
+    
+    public void getMaxTemperature(ArrayList<Double> temperatures) //Fixed method name and return type
     {
-        for (double temp: temps)
+        int count = 0;
+        double max = 0;
+        for(double temps : temperatures)
         {
-            if (temp > 37,5)
+            if(temps > 37.5)
             {
-                double max = temp;
+                
             }
         }
-        if (max < temp)
-        {
-            double newMax = temp;
-            return newMax;
-        }
+        System.out.println(count);
+        System.out.println(max);
     }
-       
-       */
       
     //question 26
     //temps.max();
